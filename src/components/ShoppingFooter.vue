088dd66d8d3d869b6c450368cd2dc8df428a278a
eva-chu <eva903942@gmail.com>
2020-10-27 19:08:31 +0800

    Create ShoppingFooter.vue

---
+++ src/components/ShoppingFooter.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>   
+        <div class="py-5 text-center row justify-content-around align-content-center background-img6 text-white">
+        <div class="col">
+            <div class="row justify-content-around align-items-center">
+                <div class="col-md">
+                    <h6>Tel : (02)5266-5266(代表號)</h6>            
+                </div>
+                <div class="col-md">
+                    <h6>Fax : (02)5266-1314</h6>
+                </div>
+                <div class="col-md">
+                    <h6>E-mail : nicetomeatyou@gmail.com.tw</h6>
+                </div>
+            </div>
+            <div class="row justify-content-around align-items-center">
+                <div class="col">
+                    <h6>Copyright © 1988 NICE TO MEAT YOU Co., Ltd.  版權所有</h6>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        methods: {
+            
+        },
+        computed: {
+            
+        }
+    }
+</script>
+
+<style>
+.background-img6 {
+    background-color: black;
+    z-index: -999;
+}
+</style>
